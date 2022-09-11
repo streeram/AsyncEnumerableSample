a057e2a98e0e8becd2aa0308bb85ab34b86e52a7
Sriram Narayanan <streeram@gmail.com>
2022-09-12 09:47:42 +1000

    Inital commit

---
+++ SampleApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async IAsyncEnumerable<WeatherForecast> Get()
+    {
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateTime.Now.AddDays(index),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToAsyncEnnumerable();
+        for (int i = 1; i <= 10; i++)
+        {
+            await Task.Delay(500);
+            yield return (new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(i),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            });
+        }
+    }
+}
---
+++ SampleClient/SampleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using SampleClient;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://localhost:7155/");
+        
+        var _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        using (var response = await _httpClient.GetAsync("WeatherForecast", HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+        {
+            response.EnsureSuccessStatusCode();
+            var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false); ;
+
+            IAsyncEnumerable<WeatherForecast> weatherForecasts = JsonSerializer.DeserializeAsyncEnumerable<WeatherForecast>(
+            stream,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                DefaultBufferSize = 128
+            });
+
+            await foreach (WeatherForecast weatherForecast in weatherForecasts)
+            {
+                System.Console.WriteLine(weatherForecast?.Summary);
+            }
+
+        }
+
+    }
+}
+
+
